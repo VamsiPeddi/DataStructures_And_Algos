6ee128c36f9b9e89325dfcabdb0b3866776ec9e0
VamsiPeddi <50645719+VamsiPeddi@users.noreply.github.com>
2022-02-20 19:31:26 -0600

    Add files via upload

---
+++ Data Structures and Algorithms/Linked_List/LinkedListCycle.java
@@ -0,0 +1,28 @@
+//Given a linked list, determine if it has a cycle in it.
+
+// Definition for singly-linked list.
+ class ListNode {
+     int val;
+     ListNode next;
+     ListNode(int x) {
+         val = x;
+         next = null;
+     }
+ }
+
+public class LinkedListCycle {
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next == null) {
+            return false;
+        }
+        
+        ListNode slow = head;
+        ListNode fast = head.next;
+        while(fast != null && fast.next != null && fast != slow) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        
+        return fast == slow;
+    }
+}
